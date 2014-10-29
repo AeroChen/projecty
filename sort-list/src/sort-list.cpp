a47b7f03a1416293a6d6969d7b1cd4345103e11c
Xichen Shi <sxc901231@gmail.com>
2014-10-28 23:05:00 -0500

    Add sort-list using MergeSort for linked-list

---
+++ sort-list/src/sort-list.cpp
@@ -0,0 +1,96 @@
+//============================================================================
+// Name        : sort-list.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+void printList(ListNode *head)
+{
+	while(head)
+	{
+		cout<<head->val<<":";
+		head = head->next;
+	}
+	cout<<endl;
+}
+
+ListNode *fillList(vector<int> input)
+{
+	if(input.empty()) return NULL;
+	ListNode *head = new ListNode(input[0]);
+	ListNode *last = head;
+	for(int i=1; i<input.size();i++)
+	{
+		ListNode *curr = new ListNode(input[i]);
+		last->next = curr;
+		last = curr;
+	}
+	return head;
+}
+
+ListNode *splitList(ListNode *head)
+{
+    if(head == NULL || head->next == NULL) return head;
+    ListNode *fast = head;
+    ListNode *slow = head;
+    ListNode *prev = slow;
+
+    while(fast)
+    {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next ? fast->next->next : NULL;
+    }
+    prev->next = NULL;
+    return slow;
+}
+
+ListNode *mergeList(ListNode *l1, ListNode *l2)
+{
+    ListNode dummy(-1);
+    ListNode *curr = &dummy;
+    while(l1 && l2)
+    {
+        if(l1->val >= l2->val)
+        {
+            curr->next = l2;
+            l2 = l2->next;
+        }
+        else
+        {
+            curr->next = l1;
+            l1 = l1->next;
+        }
+        curr = curr->next;
+    }
+    curr->next = l1? l1:l2;
+    return dummy.next;
+}
+
+ListNode *sortList(ListNode *head) {
+    if(head == NULL || head->next == NULL) return head;
+    ListNode *headref = head;
+    ListNode *halfref = splitList(headref);
+    headref = sortList(headref);
+    halfref = sortList(halfref);
+    return mergeList(headref, halfref);
+}
+
+int main() {
+	int list[] = {4,2,3,1};
+	ListNode* head = fillList(vector<int>(list, list + sizeof(list)/sizeof(int)));
+	printList(sortList(head));
+	return 0;
+}
